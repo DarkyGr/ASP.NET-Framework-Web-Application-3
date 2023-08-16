ff4ae74c919e693ed29f26087e07b96056c10d6a
Guillermo Riaño <guilledark17@gmail.com>
2023-08-16 10:00:59 -0600

    ✅ Mantenimientos - Edit & Delete

--- MVC_Proyecto_GRM/Controllers/MantenimientosController.cs
+++ MVC_Proyecto_GRM/Controllers/MantenimientosController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_Proyecto_GRM.Models.ViewModels.Mantenimientos;
+using static MVC_Proyecto_GRM.Models.Enum;
 
 namespace MVC_Proyecto_GRM.Controllers
 {
@@ -57,16 +58,102 @@ namespace MVC_Proyecto_GRM.Controllers
                         // Se agrega el nuevo camion
                         db.Mantenimientos.Add(manteni);
                         db.SaveChanges();
+                        Alert("El Mantenimiento ha sido agregado", NoticationType.success);
                     }
                     // Redirecciona a la lista de camiones (Controller)
                     return Redirect("~/Mantenimientos");
                 }
+                Alert("Verificar la información", NoticationType.warning);
                 return View(model);
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
             }
         }
+
+        public ActionResult MantenimientoEditar(int id)
+        {
+            Mantenimientos mantenimiento = new Mantenimientos();
+
+            using (RentaCarrosEntities db = new RentaCarrosEntities())
+            {
+                //camion = db.Camiones.Where(x => x.CamionId == camionId).OrderBy(x => x.Matricula).FirstOrDefault();
+                mantenimiento = db.Mantenimientos.Where(x => x.MantenimientoId == id).FirstOrDefault();
+            }
+
+            ViewBag.Title = "Editando mantenimiento con ID: " + mantenimiento.MantenimientoId;
+
+            return View(mantenimiento);
+        }
+
+        [HttpPost]
+        public ActionResult MantenimientoEditar(Mantenimientos model)
+        {
+            try
+            {
+                // Validar si modelo es correcto
+                if (ModelState.IsValid)
+                {
+                    using (RentaCarrosEntities db = new RentaCarrosEntities())
+                    {
+                        var mantenimiento = new Mantenimientos();
+
+                        mantenimiento.MantenimientoId = model.MantenimientoId;
+                        mantenimiento.VehiculoId = model.VehiculoId;
+                        mantenimiento.Nota = model.Nota;
+                        mantenimiento.Fecha = model.Fecha;
+
+                        // Se agrega el nuevo camion                        
+                        db.Entry(mantenimiento).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                        Alert("El Mantenimiento ha sido agregado", NoticationType.success);
+                    }
+                    // Redirecciona a la lista de camiones
+                    return Redirect("~/Mantenimientos");
+                }
+                Alert("Verificar la información", NoticationType.warning);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
+                //throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult MantenimientoEliminar(int id)
+        {
+            Mantenimientos mantenimiento = new Mantenimientos();
+            try
+            {
+                using (RentaCarrosEntities db = new RentaCarrosEntities())
+                {
+                    mantenimiento = db.Mantenimientos.Where(x => x.MantenimientoId == id).FirstOrDefault();
+                    db.Mantenimientos.Remove(mantenimiento);
+                    db.SaveChanges();
+                }
+                Alert("Mantenimiento Eliminado con éxito.", NoticationType.success);
+                return Redirect("~/Mantenimientos");
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return Redirect("~/Mantenimientos");
+            }
+        }
+
+        public void Alert(string message, NoticationType noticationType)
+        {
+            var msg = "<script language='javascript'>Swal.fire('" + noticationType.ToString().ToUpper() + "', '" + message +
+                "','" + noticationType + "')" + "</script>";
+
+            TempData["notification"] = msg;
+        }
+
     }
 }
