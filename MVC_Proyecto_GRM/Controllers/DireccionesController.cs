51663b81a7060e1c41471d2f5bc6fc8940391439
Guillermo Riaño <guilledark17@gmail.com>
2023-08-16 10:00:03 -0600

    ✅ Direcciones - Edit & Delete

--- MVC_Proyecto_GRM/Controllers/DireccionesController.cs
+++ MVC_Proyecto_GRM/Controllers/DireccionesController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static MVC_Proyecto_GRM.Models.Enum;
 
 namespace MVC_Proyecto_GRM.Controllers
 {
@@ -53,7 +54,7 @@ namespace MVC_Proyecto_GRM.Controllers
                     {
                         var direccion = new Direcciones();
 
-                        direccion.DireccionId = model.DireccionId; 
+                        direccion.DireccionId = model.DireccionId;
                         direccion.Calle = model.Calle;
                         direccion.Numero = model.Numero;
                         direccion.Colonia = model.Colonia;
@@ -65,16 +66,106 @@ namespace MVC_Proyecto_GRM.Controllers
                         // Se agrega el nuevo camion
                         db.Direcciones.Add(direccion);
                         db.SaveChanges();
+                        Alert("La Dirección ha sido agregada", NoticationType.success);
                     }
                     // Redirecciona a la lista de camiones (Controller)
                     return Redirect("~/Direcciones");
                 }
+                Alert("Verificar la información", NoticationType.warning);
                 return View(model);
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
             }
         }
+
+        public ActionResult DireccionEditar(int id)
+        {
+            Direcciones direccion = new Direcciones();
+
+            using (RentaCarrosEntities db = new RentaCarrosEntities())
+            {
+                //camion = db.Camiones.Where(x => x.CamionId == camionId).OrderBy(x => x.Matricula).FirstOrDefault();
+                direccion = db.Direcciones.Where(x => x.DireccionId == id).FirstOrDefault();
+            }
+
+            ViewBag.Title = "Editando dirección con ID: " + direccion.DireccionId;
+
+            return View(direccion);
+        }
+
+        [HttpPost]
+        public ActionResult DireccionEditar(Direcciones model)
+        {
+            try
+            {
+                // Validar si modelo es correcto
+                if (ModelState.IsValid)
+                {
+                    using (RentaCarrosEntities db = new RentaCarrosEntities())
+                    {
+                        var direccion = new Direcciones();
+
+                        direccion.DireccionId = model.DireccionId;
+                        direccion.Calle = model.Calle;
+                        direccion.Numero = model.Numero;
+                        direccion.Colonia = model.Colonia;
+                        direccion.CP = model.CP;
+                        direccion.Municipio = model.Municipio;
+                        direccion.Ciudad = model.Ciudad;
+                        direccion.Estado = model.Estado;
+
+                        // Se agrega el nuevo camion                        
+                        db.Entry(direccion).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                        Alert("La Dirección ha sido agregado", NoticationType.success);
+                    }
+                    // Redirecciona a la lista de camiones
+                    return Redirect("~/Direcciones");
+                }
+                Alert("Verificar la información", NoticationType.warning);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
+                //throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DireccionEliminar(int id)
+        {
+            Direcciones direccion = new Direcciones();
+            try
+            {
+                using (RentaCarrosEntities db = new RentaCarrosEntities())
+                {
+                    direccion = db.Direcciones.Where(x => x.DireccionId == id).FirstOrDefault();
+                    db.Direcciones.Remove(direccion);
+                    db.SaveChanges();
+                }
+                Alert("Dirección Eliminada con éxito.", NoticationType.success);
+                return Redirect("~/Direcciones");
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return Redirect("~/Direcciones");
+            }
+        }
+
+        public void Alert(string message, NoticationType noticationType)
+        {
+            var msg = "<script language='javascript'>Swal.fire('" + noticationType.ToString().ToUpper() + "', '" + message +
+                "','" + noticationType + "')" + "</script>";
+
+            TempData["notification"] = msg;
+        }
+
     }
 }
