5efd147a453e24c4de2dbac60a14ac6c31cff008
Guillermo Riaño <guilledark17@gmail.com>
2023-08-16 11:02:17 -0600

    ✅ Vehiculos - Edit & Delete

--- MVC_Proyecto_GRM/Controllers/VehiculosController.cs
+++ MVC_Proyecto_GRM/Controllers/VehiculosController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_Proyecto_GRM.Models.ViewModels.Vehiculos;
+using static MVC_Proyecto_GRM.Models.Enum;
 
 namespace MVC_Proyecto_GRM.Controllers
 {
@@ -61,16 +62,104 @@ namespace MVC_Proyecto_GRM.Controllers
                         // Se agrega el nuevo camion
                         db.Vehiculos.Add(vehiculo);
                         db.SaveChanges();
+
+                        Alert("El Vehículo ha sido agregado", NoticationType.success);
                     }
                     // Redirecciona a la lista de camiones (Controller)
                     return Redirect("~/Vehiculos");
                 }
+                Alert("Verificar la información", NoticationType.warning);
                 return View(model);
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
             }
+        }
+
+        public ActionResult VehiculoEditar(int id)
+        {
+            Vehiculos vehiculo = new Vehiculos();
+
+            using (RentaCarrosEntities db = new RentaCarrosEntities())
+            {
+                //camion = db.Camiones.Where(x => x.CamionId == camionId).OrderBy(x => x.Matricula).FirstOrDefault();
+                vehiculo = db.Vehiculos.Where(x => x.VehiculoId == id).FirstOrDefault();
+            }
+
+            ViewBag.Title = "Editando cliente con ID: " + vehiculo.VehiculoId;
+
+            return View(vehiculo);
+        }
+
+        [HttpPost]
+        public ActionResult VehiculoEditar(Vehiculos model)
+        {
+            try
+            {
+                // Validar si modelo es correcto
+                if (ModelState.IsValid)
+                {
+                    using (RentaCarrosEntities db = new RentaCarrosEntities())
+                    {
+                        var vehiculo = new Vehiculos();
+
+                        vehiculo.VehiculoId = model.VehiculoId;
+                        vehiculo.Matricula = model.Matricula;
+                        vehiculo.Marca = model.Marca;
+                        vehiculo.Modelo = model.Modelo;
+                        vehiculo.Capacidad = model.Capacidad;
+                        vehiculo.Kilometraje = model.Kilometraje;
+
+                        // Se agrega el nuevo camion                        
+                        db.Entry(vehiculo).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                        Alert("El Vehículo ha sido agregado", NoticationType.success);
+                    }
+                    // Redirecciona a la lista de camiones
+                    return Redirect("~/Vehiculos");
+                }
+                Alert("Verificar la información", NoticationType.warning);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return View(model);
+                //throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult VehiculoEliminar(int id)
+        {
+            Vehiculos vehiculo = new Vehiculos();
+            try
+            {
+                using (RentaCarrosEntities db = new RentaCarrosEntities())
+                {
+                    vehiculo = db.Vehiculos.Where(x => x.VehiculoId == id).FirstOrDefault();
+                    db.Vehiculos.Remove(vehiculo);
+                    db.SaveChanges();
+                }
+                Alert("Vehículo Eliminado con éxito.", NoticationType.success);
+                return Redirect("~/Vehiculos");
+            }
+            catch (Exception ex)
+            {
+                Alert("Error: " + ex.Message, NoticationType.error);
+                return Redirect("~/Vehiculos");
+            }
+        }
+
+        public void Alert(string message, NoticationType noticationType)
+        {
+            var msg = "<script language='javascript'>Swal.fire('" + noticationType.ToString().ToUpper() + "', '" + message +
+                "','" + noticationType + "')" + "</script>";
+
+            TempData["notification"] = msg;
         }
     }
 }
