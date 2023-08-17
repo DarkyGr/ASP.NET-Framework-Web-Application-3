b51a6f79ceeb88b988d3578129f22d1c9042b6df
Guillermo Riaño <guilledark17@gmail.com>
2023-08-17 09:05:26 -0600

    ✅ Vehiculos - LinQ, CargarDDL Mehtod & View

---
+++ MVC_Proyecto_GRM/Models/DDLs/VehiculosDDL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace MVC_Proyecto_GRM.Models.DDLs
+{
+    public class VehiculosDDL
+    {
+        public int VehiculoId { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Vehículo")]   // Manda el name de lo que falta
+        public string Info { get; set; }
+    }
+}
--- MVC_Proyecto_GRM/Models/Vehiculos.cs
+++ MVC_Proyecto_GRM/Models/Vehiculos.cs
@@ -11,7 +11,8 @@ namespace MVC_Proyecto_GRM.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Vehiculos
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,10 +23,25 @@ namespace MVC_Proyecto_GRM.Models
         }
     
         public int VehiculoId { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Matrícula")]   // Manda el name de lo que falta
         public string Matricula { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Marca")]   // Manda el name de lo que falta
         public string Marca { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Modelo")]   // Manda el name de lo que falta
         public string Modelo { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Capacidad")]   // Manda el name de lo que falta
         public int Capacidad { get; set; }
+
+        [Required]  // Data validator
+        [Display(Name = "Kilometraje")]   // Manda el name de lo que falta
         public double Kilometraje { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
