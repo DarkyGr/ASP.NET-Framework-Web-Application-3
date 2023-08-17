0009af66cd1594c6ca22e3d6ad996996cb51a163
Guillermo Riaño <guilledark17@gmail.com>
2023-08-17 09:07:55 -0600

    ✔️ Presentation HTML, CSS & JS

---
+++ MVC_Proyecto_GRM/Scripts/Mine.js
@@ -0,0 +1,13 @@
+﻿let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 100,
+});
+
+typewriter
+    .pauseFor(500)
+    .typeString('Renta de Carros')
+    .pauseFor(500)
+    .deleteChars(10)
+    .start();
